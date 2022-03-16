9a6b455d2f3ac30b525a9dbb3a9a86a89ac58d89
pupiltong <earth.diqiu@gmail.com>
2022-03-16 11:00:50 +0800

    stage

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 import * as tls from 'tls'
 import * as tcp from 'net'
-import { PassThrough, Transform } from "stream";
-import { likeARealTlsOption } from '.';
+import { Transform } from "stream";
+import { likeARealTlsOption } from './options';
 import { createCipheriv, createDecipheriv, randomBytes } from 'crypto';
 
 // const fakeDomain  = 'www.microsoft.com';
 const debug = true;
 const activateLikeARealTlsServer = function(options:likeARealTlsOption, tcpServer: tcp.Server){
-    const algthroim = options.algthroim;
+    const algorithm = options.algorithm;
     const oneTimeEncryptKey = options.oneTimeEncryptKey;
     const ivLength = options.ivLength;
-    const tlsApplicationDataHeader = Buffer.from(options.tlsApplicationDataHeader);
+    const tlsApplicationDataHeader = options.tlsApplicationDataHeader;
     const fakeDomain = options.fakeDomain
     const ca = options.ca;
     const cert = options.cert;
@@ -57,11 +57,11 @@ const activateLikeARealTlsServer = function(options:likeARealTlsOption, tcpServe
                 if(maybeApplicationHeader.compare(tlsApplicationDataHeader)===0){
                     const maybeIv = data.subarray(tlsApplicationDataHeader.length + 1, ivLength + tlsApplicationDataHeader.length + 1);
                     if(maybeIv.length === ivLength){
-                        const decryption = createDecipheriv(algthroim, oneTimeEncryptKey, maybeIv);
+                        const decryption = createDecipheriv(algorithm, oneTimeEncryptKey, maybeIv);
                         decryption.on('data', (decryptedData) => {
                             tcpTransFormerToReal.emit('data',decryptedData);
                         })
-                        decryption.write(data.subarray(tlsApplicationDataHeader.length + ivLength + 1, data.length));
+                        decryption.write(data.subarray(tlsApplicationDataHeader.length + ivLength + 3, data.length));
                     }
                 }
             }
@@ -75,9 +75,10 @@ const activateLikeARealTlsServer = function(options:likeARealTlsOption, tcpServe
             if(status === connectionStatues.handshaking){
                 //do replace
                 const iv = randomBytes(options.ivLength);
-                const encryption = createCipheriv(algthroim, oneTimeEncryptKey, iv);
+                const encryption = createCipheriv(algorithm, oneTimeEncryptKey, iv);
                 encryption.on('data', (data) => {
-                    const encryptedData = Buffer.concat([tlsApplicationDataHeader, iv, data]);
+                    const applicationDataLength = Buffer.alloc(2 , iv.length + data.length);
+                    const encryptedData = Buffer.concat([tlsApplicationDataHeader, applicationDataLength, iv, data]);
                     socket.write(encryptedData);
                 })
                 encryption.write(chunk,encoding,cb);
