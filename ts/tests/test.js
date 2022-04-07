83bc30fcc95507036bcf2cfc6d0809f8f543a451
PupilTong <12288479+PupilTong@users.noreply.github.com>
2022-04-07 21:59:41 +0800

    adding test cases

--- ts/tests/test.js
+++
@@ -1,3 +0,0 @@
-import * as latc from '../dist/index.js'
-import * as should from 'should';
-console.log('aaa');
---
+++ ts/tests/testTlsPacketParasr.ts
@@ -0,0 +1,13 @@
+import { TlsPacketParser } from "../src/TlsPacketParser";
+import * as should from 'should'
+import * as mocha from 'mocha'
+mocha.describe("",()=>{
+    const streamData = [
+        Buffer.from([]),
+        Buffer.from([]),
+        Buffer.from([])
+    ]
+    it("",(done)=>{
+
+    })
+})
