e78a495387f5cf3233f89b979cd1c8f1c7018937
PupilTong <12288479+PupilTong@users.noreply.github.com>
2022-04-09 21:55:20 +0800

    add test configuration

--- ts/tests/testTlsPacketParasr.ts
+++ ts/tests/testTlsPacketParasr.ts
@@ -1,13 +1,19 @@
-import { TlsPacketParser } from "../src/TlsPacketParser";
-import * as should from 'should'
-import * as mocha from 'mocha'
-mocha.describe("",()=>{
-    const streamData = [
-        Buffer.from([]),
-        Buffer.from([]),
-        Buffer.from([])
-    ]
-    it("",(done)=>{
-
+import { TlsPacketParser } from "../src/TlsPacketParser.js";
+import should from 'should'
+describe("Test Tls PacketParser",()=>{
+    let parser : TlsPacketParser;
+    beforeEach(()=>{
+        parser = new TlsPacketParser();
+    })
+    it("test one packet",(done)=>{
+        const data = [0x17, 0x03, 0x03, 0x00, 0x03, 0x01, 0x02, 0x03]
+        parser.on('data',(data)=>{
+            should.equal(Buffer.compare(data, Buffer.from(data)),0);
+            done();
+        })
+        parser.write(Buffer.from(data))
+    })
+    afterEach(()=>{
+        parser.destroy();
     })
 })
