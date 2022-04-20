cabb1e0caacc8d92b630339780580f431924a52a
PupilTong <12288479+PupilTong@users.noreply.github.com>
2022-04-20 16:15:46 +0800

    format

--- src/LatcClientSocket.ts
+++ src/LatcClientSocket.ts
@@ -1,4 +1,11 @@
-import { Transform, Duplex, TransformCallback, PassThrough, Writable, Readable } from "stream";
+import {
+    Transform,
+    Duplex,
+    TransformCallback,
+    PassThrough,
+    Writable,
+    Readable,
+} from "stream";
 import * as tls from "tls";
 import * as tcp from "net";
 import * as udp from "dgram";
@@ -8,96 +15,111 @@ import { TlsPacketParser } from "./TlsPacketParser.js";
 import lodash from "lodash";
 
 interface _LatcClientSocketOptions extends Duplex {
-    readonly port: number,
-    readonly host: string,
-    readonly hmacAlgorithm : "sha256" | "sha512",
-    readonly salt: string,
-    getHmacAlgorithm:()=>"sha256" | "sha512",
-    getSalt:()=>string | Buffer,
-    getFakeTlsSocket:()=>tls.TLSSocket
+    readonly port: number;
+    readonly host: string;
+    readonly hmacAlgorithm: "sha256" | "sha512";
+    readonly salt: string;
+    getHmacAlgorithm: () => "sha256" | "sha512";
+    getSalt: () => string | Buffer;
+    getFakeTlsSocket: () => tls.TLSSocket;
 }
 interface LatcClientSocket extends Duplex {
-    readonly port: number,
-    readonly host: string,
-    readonly hmacAlgorithm : "sha256" | "sha512",
-    readonly salt: string,
-    getHmacAlgorithm:()=>"sha256" | "sha512",
-    getSalt:()=>string | Buffer,
-    getFakeTlsSocket:()=>tls.TLSSocket
+    readonly port: number;
+    readonly host: string;
+    readonly hmacAlgorithm: "sha256" | "sha512";
+    readonly salt: string;
+    getHmacAlgorithm: () => "sha256" | "sha512";
+    getSalt: () => string | Buffer;
+    getFakeTlsSocket: () => tls.TLSSocket;
 }
 function createClientSocket(
     port: number,
     host: string,
     hmacAlgorithm: "sha256" | "sha512",
     salt: string | Buffer,
-    fakeTlsSocketOption?: tls.ConnectionOptions
+    fakeTlsSocketOption?: tls.ConnectionOptions,
 ): Promise<LatcClientSocket> {
     return new Promise((resolve, reject) => {
-        try{
-            const tcpSocket = tcp.createConnection(port, host, function(){
+        try {
+            const tcpSocket = tcp.createConnection(port, host, function () {
                 const packetParser = new TlsPacketParser();
                 const toBeLabeledStream = new PassThrough({ objectMode: true });
-                const receivedLabeledStream = new PassThrough({ objectMode: true });
+                const receivedLabeledStream = new PassThrough({
+                    objectMode: true,
+                });
                 const toBePassThroughStream = new PassThrough();
                 const receivedPassThroughStream = new PassThrough();
-                const mixer = new Mixer(hmacAlgorithm, salt, toBeLabeledStream, toBePassThroughStream);
+                const mixer = new Mixer(
+                    hmacAlgorithm,
+                    salt,
+                    toBeLabeledStream,
+                    toBePassThroughStream,
+                );
                 mixer.pipe(tcpSocket);
-                const separator = new Separator(hmacAlgorithm, salt, receivedLabeledStream, receivedPassThroughStream);
+                const separator = new Separator(
+                    hmacAlgorithm,
+                    salt,
+                    receivedLabeledStream,
+                    receivedPassThroughStream,
+                );
                 packetParser.pipe(separator);
                 tcpSocket.pipe(packetParser);
 
                 const toFakeTlsStream = Duplex.from({
-                    writable:toBePassThroughStream,
-                    readable:receivedPassThroughStream,
+                    writable: toBePassThroughStream,
+                    readable: receivedPassThroughStream,
                 });
-                const fakeTlsSocket = tls.connect({
-                                ...fakeTlsSocketOption,
-                                socket: toFakeTlsStream,
-                },()=>{
-                    const latcClientSocket = Duplex.from({
-                        writable: toBeLabeledStream,
-                        readable: receivedLabeledStream
-                    }) as LatcClientSocket;
+                const fakeTlsSocket = tls.connect(
+                    {
+                        ...fakeTlsSocketOption,
+                        socket: toFakeTlsStream,
+                    },
+                    () => {
+                        const latcClientSocket = Duplex.from({
+                            writable: toBeLabeledStream,
+                            readable: receivedLabeledStream,
+                        }) as LatcClientSocket;
 
-                    latcClientSocket.getHmacAlgorithm = function(){
-                        return hmacAlgorithm;
-                    }
-                    latcClientSocket.getSalt = function(){
-                        return salt;
-                    }
-                    latcClientSocket.getFakeTlsSocket = function() {
-                        return fakeTlsSocket;
-                    }
-                    latcClientSocket._destroy = function(err,cb){
-                        try{
-                            tcpSocket.destroy();
-                            latcClientSocket.destroy();
-                            toFakeTlsStream.destroy();
-                            mixer.destroy();
-                            separator.destroy();
-                            toBeLabeledStream.destroy();
-                            toBePassThroughStream.destroy();
-                            receivedLabeledStream.destroy();
-                            receivedPassThroughStream.destroy();
-                            packetParser.destroy();
-                        } catch (e){
-                            cb(e);
-                        }
-                    }
-                    tcpSocket.removeAllListeners('error');
-                    tcpSocket.on('error',(err)=>{
-                        latcClientSocket.destroy(err);
-                    })
-                    fakeTlsSocket.on('error',(err)=>{
-                        latcClientSocket.destroy(err);
-                    })
-                    resolve(latcClientSocket);
-                });
+                        latcClientSocket.getHmacAlgorithm = function () {
+                            return hmacAlgorithm;
+                        };
+                        latcClientSocket.getSalt = function () {
+                            return salt;
+                        };
+                        latcClientSocket.getFakeTlsSocket = function () {
+                            return fakeTlsSocket;
+                        };
+                        latcClientSocket._destroy = function (err, cb) {
+                            try {
+                                tcpSocket.destroy();
+                                latcClientSocket.destroy();
+                                toFakeTlsStream.destroy();
+                                mixer.destroy();
+                                separator.destroy();
+                                toBeLabeledStream.destroy();
+                                toBePassThroughStream.destroy();
+                                receivedLabeledStream.destroy();
+                                receivedPassThroughStream.destroy();
+                                packetParser.destroy();
+                            } catch (e) {
+                                cb(e);
+                            }
+                        };
+                        tcpSocket.removeAllListeners("error");
+                        tcpSocket.on("error", (err) => {
+                            latcClientSocket.destroy(err);
+                        });
+                        fakeTlsSocket.on("error", (err) => {
+                            latcClientSocket.destroy(err);
+                        });
+                        resolve(latcClientSocket);
+                    },
+                );
             });
-            tcpSocket.once('error',e=>{
+            tcpSocket.once("error", (e) => {
                 reject(e);
-            })
-        } catch (e){
+            });
+        } catch (e) {
             reject(e);
         }
     });
--- test/testLatc.ts
+++ test/testLatc.ts
@@ -1,71 +1,80 @@
-import * as tcp from 'net'
-import * as tls from 'tls'
-import { createServerSocket } from '../src/LatcServerSocket.js'
-import { createClientSocket } from '../src/LatcClientSocket.js'
-import should from 'should';
-describe("Test Latc",()=>{
-    let tcpServer : tcp.Server;
-    const fakeName  = "www.icloud.com";
-    beforeEach(function(){
+import * as tcp from "net";
+import * as tls from "tls";
+import { createServerSocket } from "../src/LatcServerSocket.js";
+import { createClientSocket } from "../src/LatcClientSocket.js";
+import should from "should";
+describe("Test Latc", () => {
+    let tcpServer: tcp.Server;
+    const fakeName = "www.icloud.com";
+    beforeEach(function () {
         tcpServer = new tcp.Server();
-        tcpServer.listen(2222,'127.0.0.1');
+        tcpServer.listen(2222, "127.0.0.1");
         this.timeout(6000);
-    })
+    });
 
-    it("request fake server cert",function(done){
-        tcpServer.once('connection',(serverSocket)=>{
-            createServerSocket(serverSocket,443,fakeName,"sha256","salt").then(server=>{
-            })
-        })
-        createClientSocket(2222,'127.0.0.1',"sha256","salt",{
-            servername:fakeName,
-            rejectUnauthorized:true,
+    it("request fake server cert", function (done) {
+        tcpServer.once("connection", (serverSocket) => {
+            createServerSocket(
+                serverSocket,
+                443,
+                fakeName,
+                "sha256",
+                "salt",
+            ).then((server) => {});
+        });
+        createClientSocket(2222, "127.0.0.1", "sha256", "salt", {
+            servername: fakeName,
+            rejectUnauthorized: true,
             // enableTrace:true,
-            checkServerIdentity:(hostname,cert)=>{
-                const result = tls.checkServerIdentity(fakeName,cert);
-                should.equal(result,undefined);
+            checkServerIdentity: (hostname, cert) => {
+                const result = tls.checkServerIdentity(fakeName, cert);
+                should.equal(result, undefined);
                 done();
                 return result;
-            }
-        }).then(socket=>{
-            socket.on('error',()=>{});
+            },
+        }).then((socket) => {
+            socket.on("error", () => {});
             socket.destroy();
         });
     });
 
-
-    it("client to server",(done)=>{
-        const msg = "hello world"
-        tcpServer.on('connection',(serverSocket)=>{
-            createServerSocket(serverSocket,443,fakeName,"sha256","salt").then(server=>{
-                server.once('data',data=>{
-                    if(data.toString() == msg){
+    it("client to server", (done) => {
+        const msg = "hello world";
+        tcpServer.on("connection", (serverSocket) => {
+            createServerSocket(
+                serverSocket,
+                443,
+                fakeName,
+                "sha256",
+                "salt",
+            ).then((server) => {
+                server.once("data", (data) => {
+                    if (data.toString() == msg) {
                         // server.destroy();
                         done();
                     }
-                })
-            })
-        })
-        createClientSocket(2222,'127.0.0.1',"sha256","salt",{
-            servername:fakeName,
-            rejectUnauthorized:true,
-        }).then(client=>{
+                });
+            });
+        });
+        createClientSocket(2222, "127.0.0.1", "sha256", "salt", {
+            servername: fakeName,
+            rejectUnauthorized: true,
+        }).then((client) => {
             client.write(msg);
             client.destroy();
         });
     });
 
-    
-    it("catch an error while creating client",function(done){
-        createClientSocket(54321,'127.0.0.1',"sha256","salt",{
-            servername:fakeName,
-            rejectUnauthorized:true,
-        }).catch(e=>{
+    it("catch an error while creating client", function (done) {
+        createClientSocket(54321, "127.0.0.1", "sha256", "salt", {
+            servername: fakeName,
+            rejectUnauthorized: true,
+        }).catch((e) => {
             done();
-        })
+        });
     });
-    
-    afterEach(()=>{
+
+    afterEach(() => {
         tcpServer.close();
-    })
-})
+    });
+});
